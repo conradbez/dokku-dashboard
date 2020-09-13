54d7df0fb5048bacdf311e8c57d7999f04c9943e
Ubuntu <ubuntu@ip-172-31-19-20.us-west-2.compute.internal>
2020-09-13 09:48:25 +0000

    deploying

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ cat ./.ssh.pub | ssh root@$dokku_host dokku ssh-keys:add dokkudashboard
 ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku --force apps:destroy dashboard
 ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku apps:create dashboard
 
+# deploy our dashboard app
 git remote add dokku dokku@$dokku_host:dashboard
-
-
 git add *
+# need to add these so our dashboared app has the connection details to execute commands on dokku
 git add --force .ssh dokku_host.config
 git commit -m "deploying"
 git push dokku master
