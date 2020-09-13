7f605a29b40e3c800ce982a92047a4613a1669c7
Ubuntu <ubuntu@ip-172-31-19-20.us-west-2.compute.internal>
2020-09-13 09:22:42 +0000

    deploying

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,22 @@
-ssh-keygen -b 2048 -t rsa -f ./.ssh -q -N "ThisIsMyLoginPassword"
 
-ssh root@128.199.180.162 dokku ssh-keys:remove streamlitdokkugui
-cat ./.ssh.pub | ssh root@128.199.180.162 dokku ssh-keys:add streamlitdokkugui
+echo Enter a secure password you will use to access your dokku instance \(will be used to encrypt your ssh key\)
 
-ssh  -o StrictHostKeyChecking=no root@128.199.180.162 dokku apps:create gui
+read user_password
+echo  $user_password
 
-git remote add dokku dokku@128.199.180.162:gui
+echo Enter a your dokku domain or IP address
+
+read dokku_host
+echo  $dokku_host
+
+
+ssh root@$dokku_host dokku ssh-keys:remove streamlitdokkugui
+cat ./.ssh.pub | ssh root@$dokku_host dokku ssh-keys:add streamlitdokkugui
+
+ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku --force apps:destroy gui
+ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku apps:create gui
+
+git remote add dokku dokku@$dokku_host:gui
 
 git add *
 git commit -m "deploying"
--- main.py
+++ main.py
@@ -2,10 +2,55 @@ import streamlit as st
 import paramiko
 from paramiko import AutoAddPolicy
 import os
-client = paramiko.SSHClient()
-key = paramiko.RSAKey.from_private_key_file('/home/ubuntu/.ssh/id_rsa')
-key = paramiko.RSAKey.from_private_key_file('.ssh','ThisIsMyLoginPassword')
+import re
+dokku_host = '128.199.180.162'
 
-client.set_missing_host_key_policy(AutoAddPolicy())
-client.connect('128.199.180.162',username='root')
 
+def cleanseUserInput(oneWordedInput):
+    return re.sub(r'\W+', '', oneWordedInput)
+
+# Get ssh connection
+ssh_key_password = st.sidebar.text_input('Password set during deployment')
+if ssh_key_password:
+    client = paramiko.SSHClient()
+    key = paramiko.RSAKey.from_private_key_file('/home/ubuntu/.ssh/id_rsa')
+    key = paramiko.RSAKey.from_private_key_file('.ssh',ssh_key_password)
+    
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    client.connect(dokku_host,username='root')
+    st.write('Connected to dokku')
+    def execD(com):
+        global client
+        stdin, stdout, stderr = client.exec_command(com)
+        return stdout.readlines()
+        
+    com = 'dokku apps:list'
+    stdout = execD(com)
+    
+    availible_apps = stdout[1:]
+    
+    selected_app = st.sidebar.selectbox('What app would you like to work with',['No app selected']+availible_apps+['Create new'])
+    
+    
+    if selected_app == 'Create new':
+        user_app_name = cleanseUserInput(st.text_input('App name:'))
+        if len(user_app_name)>0:
+            stdin, stdout, stderr = client.exec_command('dokku apps:create '+user_app_name)
+            st.write(stdout.readlines())
+            st.markdown("""Run `git remote add dokku dokku@{}:{} ` to add dokku to your git and then `git push dokku master` to deploy.""".format(dokku_host,user_app_name))
+    elif selected_app != 'No app selected' and selected_app:
+        if st.checkbox('Detroy app'):
+            stdin, stdout, stderr = client.exec_command('dokku --force apps:destroy {}'.format(selected_app))
+            st.write(stdout.readlines())
+            
+        if st.checkbox('Provision Postgress'):
+            postgress_name = st.text_input('Name the services:')
+            if len(postgress_name) > 0:
+                try:
+                    st.write(execD("dokku postgres:create {}".format(postgress_name)))
+                except:
+                    st.write(execD("sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres"))
+                    st.write(execD("dokku postgres:create {}".format(postgress_name)))
+                st.write(execD("dokku postgres:link {} {}".format(postgress_name,selected_app)))
+            
+            
