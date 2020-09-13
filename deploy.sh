d529078b2513c71f088491e04f5a807f69eb2ab4
Ubuntu <ubuntu@ip-172-31-19-20.us-west-2.compute.internal>
2020-09-13 09:45:08 +0000

    deploying

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,24 @@
-
+# Get password from user
 echo Enter a secure password you will use to access your dokku instance \(will be used to encrypt your ssh key\)
-
 read user_password
-echo  $user_password
 
+# Get host name from user
 echo Enter a your dokku domain or IP address
-
 read dokku_host
-echo  $dokku_host
+
+# Write hostname to config file, to be read by app after deployment
+rm dokku_host.config
 echo $dokku_host >> dokku_host.config
 
+# add the created ssh key
+ssh root@$dokku_host dokku ssh-keys:remove dokkudashboard
+cat ./.ssh.pub | ssh root@$dokku_host dokku ssh-keys:add dokkudashboard
 
-ssh root@$dokku_host dokku ssh-keys:remove streamlitdokkugui
-cat ./.ssh.pub | ssh root@$dokku_host dokku ssh-keys:add streamlitdokkugui
+# create the dashboard app
+ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku --force apps:destroy dashboard
+ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku apps:create dashboard
 
-ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku --force apps:destroy gui
-ssh  -o StrictHostKeyChecking=no root@$dokku_host dokku apps:create gui
-
-git remote add dokku dokku@$dokku_host:gui
+git remote add dokku dokku@$dokku_host:dashboard
 
 
 git add *
