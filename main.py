d439143960caf24fab82e9109ba72b44ad5683f8
Ubuntu <ubuntu@ip-172-31-29-75.us-west-2.compute.internal>
2020-09-15 03:42:26 +0000

    update

--- main.py
+++ main.py
@@ -14,26 +14,54 @@ with open("dokku_host.config") as f:
 def cleanseUserInput(oneWordedInput):
     return re.sub(r'\W+', '', oneWordedInput)
 
+
+
+# User actions
+def appReport(appname):
+    stdin, stdout, stderr = client.exec_command('dokku --force apps:report {}'.format(appname))
+    st.write(stdout.readlines())
+            
+
+def destroyApp(appname):
+    confirm_destruction = st.text_input("Type app name to destroy")
+    if confirm_destruction == appname:
+        if st.buttton(f'Destroy {appname}'):
+            stdin, stdout, stderr = client.exec_command('dokku --force apps:destroy {}'.format(appname))
+            st.write(stdout.readlines())
+
+def desplayAppLogs(appname):
+        stdin, stdout, stderr = client.exec_command(f'dokku logs {appname}')
+        st.write(stdout.readlines())
+
+            
+            
+def provisionPostress(appname):
+    postgress_name = cleanseUserInput(st.text_input('Name the services:'))
+    if len(postgress_name) > 0:
+        try:
+            st.write(execD("dokku postgres:create {}".format(postgress_name)))
+        except:
+            st.write(execD("sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres"))
+            st.write(execD("dokku postgres:create {}".format(postgress_name)))
+        st.write(execD("dokku postgres:link {} {}".format(postgress_name,appname)))
+    
+
 # Get ssh connection
-ssh_key_password = st.sidebar.text_input('Password set during deployment')
+ssh_key_password = st.sidebar.text_input('Password set during deployment', type="password")
 if ssh_key_password:
     client = paramiko.SSHClient()
-    # key = paramiko.RSAKey.from_private_key_file('/home/ubuntu/.ssh/id_rsa')
-    st.write(ssh_key_password)
     key = paramiko.RSAKey.from_private_key_file('.ssh',password=ssh_key_password)
     
     client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect(dokku_host,username='root')
-    st.write('Connected to dokku')
+    client.connect(dokku_host,username='root', pkey=key)
+    st.sidebar.write('Connected to dokku')
+    
     def execD(com):
         global client
         stdin, stdout, stderr = client.exec_command(com)
         return stdout.readlines()
         
-    com = 'dokku apps:list'
-    stdout = execD(com)
-    
-    availible_apps = stdout[1:]
+    availible_apps = execD('dokku --quiet apps:list')
     
     selected_app = st.sidebar.selectbox('What app would you like to work with',['No app selected']+availible_apps+['Create new'])
     
@@ -44,19 +72,28 @@ if ssh_key_password:
             stdin, stdout, stderr = client.exec_command('dokku apps:create '+user_app_name)
             st.write(stdout.readlines())
             st.markdown("""Run `git remote add dokku dokku@{}:{} ` to add dokku to your git and then `git push dokku master` to deploy.""".format(dokku_host,user_app_name))
+    
     elif selected_app != 'No app selected' and selected_app:
-        if st.checkbox('Detroy app'):
-            stdin, stdout, stderr = client.exec_command('dokku --force apps:destroy {}'.format(selected_app))
-            st.write(stdout.readlines())
-            
-        if st.checkbox('Provision Postgress'):
-            postgress_name = cleanseUserInput(st.text_input('Name the services:'))
-            if len(postgress_name) > 0:
-                try:
-                    st.write(execD("dokku postgres:create {}".format(postgress_name)))
-                except:
-                    st.write(execD("sudo dokku plugin:install https://github.com/dokku/dokku-postgres.git postgres"))
-                    st.write(execD("dokku postgres:create {}".format(postgress_name)))
-                st.write(execD("dokku postgres:link {} {}".format(postgress_name,selected_app)))
+        selected_action = st.selectbox(f'What would you like to do with: {selected_app}', [
+            'Info',
+            'Destroy app', 
+            'Manage Postgress',
+            'Logs'            
+            ])
+        if selected_action == 'Logs':
+            desplayAppLogs(selected_app)
+        
+        if selected_action == 'Info':
+            appReport(selected_app)
             
             
+        if selected_action == 'Detroy app':
+            destroyApp(selected_app)
+            
+        if selected_action == 'Provision Postgress':
+            provisionPostress(selected_app)
+            
+            
+            
+
+    
